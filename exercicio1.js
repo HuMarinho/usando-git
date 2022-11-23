1e534ac6fd2f101511f61cf7db5a3e1d51be92c5
HuMarinho <humbertombmarinho@gmail.com>
2022-11-22 21:11:59 -0300

    envio de exercicio

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+let mensageminicial = "Essa é a calculadora de IMC";
+console.log(mensageminicial);
+
+let altura = 1.92;
+let peso = 95;
+
+let altura2 = altura * altura;
+let IMC = peso / altura2;
+let mensagemFinal = `O IMC com os dados apresetados é ${IMC}`;
+console.log(mensagemFinal);
